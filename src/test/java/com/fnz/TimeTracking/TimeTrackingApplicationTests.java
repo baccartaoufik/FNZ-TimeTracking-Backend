2fd26c783ff3c908181d38a8bddd58ad795652fe
tbaccar.ext <tbaccar.ext@diamos.com>
2024-06-24 15:35:29 +0100

    project init

---
+++ src/test/java/com/fnz/TimeTracking/TimeTrackingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fnz.TimeTracking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeTrackingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
