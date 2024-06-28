a314b6fdbc8a577c068971a48c725b261ade7d55
Chamroukhi Wejdene <wejdene24chamroukhi@gmail.com>
2024-06-28 06:10:45 -0700

    FNZ-TimeTracking-2 :making changes JWT claims are the core information that JWTs transmit (kinda like the letter inside a sealed envelope).
    The JWT claims included in the payload determine which information the JWT communicates (i.e., user identity, permissions, expiration of JWT, to name a few).
    In our case, when using JWTs for authentication based on face recognition:
    -Subject (sub): This could represent the unique identifier of the user authenticated via face recognition, such as a user ID.
    -Expiration Time (exp): Specifies the time when the JWT will expire and should no longer be accepted for authentication.

--- src/main/java/com/fnz/TimeTracking/controller/AuthController.java
+++ src/main/java/com/fnz/TimeTracking/controller/AuthController.java
@@ -1,5 +1,4 @@
 package com.fnz.TimeTracking.controller;
-
 import com.fnz.TimeTracking.model.Utilisateur;
 import com.fnz.TimeTracking.service.AuthService;
 import com.fnz.TimeTracking.service.JwtUtil;
@@ -10,7 +9,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.util.Collections;
 import java.util.Map;
 
@@ -18,7 +16,6 @@ import java.util.Map;
 @RequestMapping("/auth")
 public class AuthController
 {
-
     @Autowired
     private AuthService authService;
 
@@ -28,20 +25,12 @@ public class AuthController
     @PostMapping("/login")
     public ResponseEntity<?> login( @RequestBody Map<String, String> request ) {
         String image = request.get("image");
-
-        //Calls the AuthService to validate the face and get user details
         Utilisateur userDetails = authService.validateFace(image);
 
-        // If user is validated, generate a JWT token
         if (userDetails != null) {
-
             String token = jwtUtil.generateToken(userDetails);
-
-            // Return the token in the response body with HTTP 200 OK status
             return ResponseEntity.ok( Collections.singletonMap("token", token ) );
         } else {
-
-            // If validation fails, return an HTTP 401 Unauthorized status with a message
             return ResponseEntity.status( HttpStatus.UNAUTHORIZED ).body("Invalid face recognition" );
         }
     }
--- src/main/java/com/fnz/TimeTracking/service/AuthService.java
+++ src/main/java/com/fnz/TimeTracking/service/AuthService.java
@@ -7,25 +7,19 @@ import org.springframework.web.client.RestTemplate;
 
 @Service
 public class AuthService {
-
-
     final private RestTemplate restTemplate;
 
     //@Value("${python.microservice.url}")
     private String pythonMicroserviceUrl;
 
-    //build a RestTemplate instance
     AuthService(RestTemplateBuilder restTemplateBuilder) {
         this.restTemplate = restTemplateBuilder.build();
     }
 
-    //Face Validation Method
     public Utilisateur validateFace(String image) {
         String url = pythonMicroserviceUrl + "/validate-face";
         Utilisateur user = restTemplate.postForObject(url, image, Utilisateur.class);
-
-        //Response Validation
-        if (user != null && user.isValid()) { // Assuming the Python service returns a Utilisateur object with a valid flag
+        if (user != null && user.isValid()) {
             return user;
         } else {
             return null;
