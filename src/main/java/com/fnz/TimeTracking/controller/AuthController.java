53c3a72497547053ff1c9c81c325a7ddf89e261c
tbaccar.ext <tbaccar.ext@diamos.com>
2024-07-01 10:15:35 +0100

    FNZ-TimeTracking-2 :Adding missing deps JWT claims are the core information that JWTs transmit (kinda like the letter inside a sealed envelope).

--- src/main/java/com/fnz/TimeTracking/controller/AuthController.java
+++ src/main/java/com/fnz/TimeTracking/controller/AuthController.java
@@ -5,6 +5,7 @@ import com.fnz.TimeTracking.service.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -33,5 +34,10 @@ public class AuthController
         } else {
             return ResponseEntity.status( HttpStatus.UNAUTHORIZED ).body("Invalid face recognition" );
         }
+    }
+
+    @GetMapping("/test")
+    public ResponseEntity<String> test() {
+        return ResponseEntity.status( HttpStatus.OK ).body( jwtUtil.generateToken( new Utilisateur() ) );
     }
 }
